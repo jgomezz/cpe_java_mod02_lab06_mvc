a9d2ae1c069271c86c9b1ae3c4c7b682f78ce56d
jgomezz <jgomezz@gmail.com>
2025-12-14 10:03:51 -0500

    update

--- src/main/java/pe/edu/tecsup/app/controlador/ClientesBuscarServlet.java
+++ src/main/java/pe/edu/tecsup/app/controlador/ClientesBuscarServlet.java
@@ -7,27 +7,43 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import pe.edu.tecsup.app.modelo.ClienteNegocio;
+
 /**
  * Servlet implementation class ClientesBuscarServlet
  */
 @WebServlet("/ClientesBuscarServlet")
 public class ClientesBuscarServlet extends HttpServlet {
+	
 	private static final long serialVersionUID = 1L;
-       
+    
+	private ClienteNegocio negocio;
+	
     /**
      * @see HttpServlet#HttpServlet()
      */
     public ClientesBuscarServlet() {
         super();
-        // TODO Auto-generated constructor stub
+
+        this.negocio = new ClienteNegocio();
+
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	
+
+		String codigoCliente = request.getParameter("txtcodigo");
+		
+		String ret = this.negocio.buscarCliente(codigoCliente);
+		
+		System.out.println(ret);
+		
 		response.getWriter().append("Served at: ").append(request.getContextPath());
+	
+	
 	}
 
 	/**
---
+++ src/main/java/pe/edu/tecsup/app/modelo/ClienteNegocio.java
@@ -0,0 +1,10 @@
+package pe.edu.tecsup.app.modelo;
+
+public class ClienteNegocio {
+
+	public String buscarCliente(String codigoCliente) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
